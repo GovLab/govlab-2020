cbe2f3ac4132c19d801bd861bb7fca7435c85048
Anirudh Dinesh <anirudhdinesh@Anirudhs-MacBook-Pro.local>
2020-10-30 13:58:44 -0400

    project-menu-fixed

---
+++ static/js/home-page-scroll.js
@@ -0,0 +1,16 @@
+const client=  new DirectusSDK({
+  url: "https://directus.thegovlab.com/",
+  project: "thegovlab",
+  storage: window.localStorage
+})
+
+client.getItems(
+  'blog', {
+    limit: 3,
+    sort:"-original_date",
+    fields: ['*.*','authors.team_id.*','authors.team_id.picture.*','related_posts.incoming_blog_id.*','related_publications.pub_id.*','related_publications.pub_id.picture.*','related_projects.projects_id.*','related_projects.projects_id.main_picture.*']
+  }).then(data => {
+    render(data.data);
+    // console.log(data);
+})
+.catch(error => console.error(error));
