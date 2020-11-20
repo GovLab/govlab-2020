dc39fa323ee8e3d800361a82eb85661ab3c86982
Stephan <schmid91@mgmail.com>
2020-11-20 17:00:09 -0500

    meta info change Stefaan

--- static/js/team_detail.js
+++ static/js/team_detail.js
@@ -32,24 +32,39 @@ new Vue({
       news_toggle:false,
       publications_toggle:false,
       projects_toggle:false,
-      events_toggle:false,
       meta_title:'',
       meta_content:'',
+      meta_image: '',
+      twitter_title:'',
+      twitter_image:'',
+      twitter_desc:'',
       apiURL: 'https://directus.thegovlab.com/thegovlab/items/team?filter[slug][like]=',
       apiApp: '&fields=*.*,books.books_id.*,videos.directus_files_id.*,projects.projects_id.*'
 
     }
   },
-  metaInfo () {
-        return {
-          title: this.meta_title,
-          meta: [
-            {title: this.meta_title, property:'og:title'},
-      {  name: 'description', content: this.meta_content, property:'og:description'}
-    ]
-    }
-  },
+metaInfo () {
+     return {
+       title: this.meta_title,
+       meta: [
 
+      {name: 'twitter:card', content: 'summary_large_image'},
+       {name: 'twitter:title', content: this.meta_title},
+       {name: 'twitter:description', content: this.meta_content},
+       // image must be an absolute path
+       {name: 'twitter:image', content: this.meta_image},
+       // Facebook OpenGraph
+       {property: 'og:title', content: this.meta_title},
+       {property: 'og:site_name', content: this.meta_title},
+       {property: 'og:type', content: 'website'},
+       {property: 'og:image', content:  this.meta_image},
+       {property: 'og:description', content:  this.meta_content},
+       { itemprop:'name', content: this.meta_title},
+       { itemprop:'image', content: this.meta_image},
+       { itemprop:'description', content: this.meta_content}
+ ]
+ }
+},
   created: function created() {
     this.memberslug=window.location.pathname.split('/');
     this.memberslug = this.memberslug[this.memberslug.length - 1].split('.')[0];
@@ -73,7 +88,7 @@ new Vue({
     filter: {
       slug: self.memberslug
     },
-    fields: ['*.*','books.books_id.*','videos.directus_files_id.*','books.books_id.picture.*','projects.projects_id.*','bio_events.events_id.*','bio_courses.courses_id.*']
+    fields: ['*.*','books.books_id.*','videos.directus_files_id.*','books.books_id.picture.*','projects.projects_id.*']
   }
 ).then(data => {
 
@@ -88,6 +103,7 @@ console.log(data.data[0]);
   self.memberData = data.data[0];
   self.meta_title = 'The Govlab | '+self.memberData.name;
   self.meta_content = self.memberData.bio_short;
+  self.meta_image = 'https://www.thegovlab.org/static/img/team/stefaan-verhulst.jpg';
 })
 .catch(error => console.error(error));
     },
