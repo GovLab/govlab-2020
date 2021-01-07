54b92e98bc813318eb40963413af43735e91ad77
Stephan <schmid91@gmail.com>
2021-01-07 11:23:23 -0500

    /

--- static/js/project_specific.js
+++ static/js/project_specific.js
@@ -81,7 +81,7 @@ new Vue({
       progessAr: ['m-define','m-prototype','m-test','m-complete'],
       progess:'',
       meta_title: 'The GovLab | Project',
-      meta_image: 'https://govlab.github.io/new-govlab-blog/img/govlab-sm.png',
+      meta_image: 'https://www.thegovlab.org/static/img/govlab-og.png',
       meta_content: '',
       projectslug:'',
       apiURL: 'https://directus.thegovlab.com/thegovlab/items/projects'
