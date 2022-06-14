122d9d63f3c2c148db3881ea69501678c64dc671
Stephan <schmid91@mail.com>
2022-06-14 12:08:48 -0400

    d9 sites

---
+++ static/js/d9inst.js
@@ -0,0 +1,49 @@
+////////////////////////////////////////
+// reload page after Forward and back
+///////////////////////////////////////
+
+const TYPE_BACK_FORWARD = 2;
+
+function isReloadedPage() {
+  return performance.navigation.type === TYPE_BACK_FORWARD;
+}
+
+function main() {
+  if (isReloadedPage()) {
+    window.location.reload();
+  }
+}
+main();
+
+////////////////////////////////////////////////////////////
+///// TEAM  API REQUEST ` `
+////////////////////////////////////////////////////////////
+
+
+Vue.use(VueMeta);
+new Vue({
+
+  el: '#site-page',
+
+  data () {
+    return {
+      D9InsttData: [],
+      apiURLD9: 'https://d9.thegovlab.com/'
+    }
+  },
+  created: function created() {
+
+    this.fetchD9Inst();
+  },
+  methods: {
+
+    fetchD9Inst() {
+      self = this;
+      axios.get(this.apiURLD9+"items/thegovlab_directus9_instances").then(data => {
+
+        self.D9InsttData = data.data.data;
+
+      }).catch(error => console.error(error));
+    }
+  }
+});
